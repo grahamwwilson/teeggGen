0e448a687abf9a198c00ea4ee4099d2b0dfeb56e
grahamwwilson <gwwilson@ku.edu>
2023-05-12 12:18:49 -0500

    Add comments

--- cl.sh
+++ cl.sh
@@ -6,6 +6,7 @@
 #          Graham W. Wilson    12-MAY-2023
 #
 # To compile with an alternative configuration file (for example etronOPAL)
+#
 # ./cl.sh etronOPAL
 #
 # Currently use --std=legacy. 
@@ -20,6 +21,10 @@ config=${1:-egamma}
 configfile=${config}-config.f
 rm teegg-configfile.f
 ln -s ${configfile} teegg-configfile.f
+
+# TEEGG uses CERNLIB CPU time management subroutines: TIMED, TIMEX, TIMEL, TIMEST. 
+# Presumably not really necessary - and could be dispensed with to 
+# avoid need to compile against CERNLIB.
 
 gfortran --std=legacy -o ${fn} ${fn}.f `cernlib -safe pawlib mathlib kernlib packlib`
 
